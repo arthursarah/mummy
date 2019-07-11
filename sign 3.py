9193d4a0228fe4477b062b0a139e5e5035463ae6
arthursarah <52702579+arthursarah@users.noreply.github.com>
2019-07-11 11:05:40 +0100

    Add files via upload

---
+++ sign 3.py
@@ -0,0 +1,13 @@
+def calculate():
+    a=int(input("enter a number"))
+    b=int(input("enter a number"))
+    add=a+b
+    multiply=a*b
+    divide=a/b
+    subtract=a-b
+    print(add)
+    print(multiply)
+    print(divide)
+    print(subtract)
+
+calculate()
